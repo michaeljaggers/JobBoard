058c3f73b1f6314f27fa3ab8936a1b0910cd0e74
michaeljaggers <michael.jaggers@gmail.com>
2021-06-30 20:35:37 -0500

    cleanup

--- JobBoard.UI/Controllers/HomeController.cs
+++ JobBoard.UI/Controllers/HomeController.cs
@@ -18,14 +18,6 @@ namespace JobBoard.UI.Controllers
             return View();
         }
 
-        [HttpGet]
-        [Authorize]
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
 
         [HttpGet]
         public ActionResult Contact()
