78084de67813dd22667d89465c2d88aaeb56a6cf
michaeljaggers <michael.jaggers@gmail.com>
2021-07-08 19:15:37 -0500

    Cleaned up UserAdmin CRUD views

---
+++ JobBoard.DATA/Metadata/AspNetUserMetadata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoard.DATA
+{
+    class AspNetUserMetadata
+    {
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+    }
+
+    [MetadataType(typeof(AspNetUserMetadata))]
+    public partial class AspNetUser { }
+}
